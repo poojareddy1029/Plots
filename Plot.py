8e5ac0ab5458027561a9269d1df83b1ed176edd3
poojareddy1029 <119031912+poojareddy1029@users.noreply.github.com>
2025-04-10 12:59:38 +0530

    Update Plot.py

--- Plot.py
+++ Plot.py
@@ -44,4 +44,5 @@ hist_data = [x1,x2,x3]
 group_labels= ['Group - 1', 'Group - 2', 'Group - 3']
 fig = ff.create_distplot(hist_data, group_labels, bin_size = [.1,.25,.5])
 st.plotly_chart(fig)
+print('by pooja')
 
